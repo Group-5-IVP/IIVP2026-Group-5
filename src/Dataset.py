823cd21a00379815bd9b22aba8ad196c372dcb17
Dodo2k01 <dominik.gorak@gmail.com>
2026-04-19 15:23:43 +0200

    Torch

--- src/Dataset.py
+++ src/Dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from torchvision.transforms import v2
 from PIL import Image
 
+
 class HindiDigitsDataset(Dataset):
     def __init__(self, csv_path: str, img_dir: str, transform=None):
         self.df = pd.read_csv(csv_path)
@@ -14,4 +15,16 @@ class HindiDigitsDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.df)
+
+    def __getitem__(self, idx: int):
+        row = self.df.iloc[idx]
+        img_path = self.img_dir / row["filename"]  # adjust column name if different
+        img = Image.open(img_path).convert("L")  # "L" = grayscale; use "RGB" if 3-channel
+        label = int(row["label"])  # adjust column name if different
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label
+
 
